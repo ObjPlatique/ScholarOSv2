994ee940855f766441903dc0337795821bd7eb7e
ObjPlatique <chinh0352479982@gmail.com>
2026-08-30 08:03:07 +0700

    chore: remove obsolete Drive repair module

--- public/js/features/drive-repair.js
+++
@@ -1,3 +0,0 @@
-// Legacy Drive repair is intentionally disabled.
-// Drive now has one source of truth: drive_files via drive-data.js.
-export function repairLegacyDrive() { return Promise.resolve(null); }
